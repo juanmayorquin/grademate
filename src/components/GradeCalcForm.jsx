2bfce522372252b941dc5ed02246383a9d7ecb05
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2024-11-21 13:46:21 -0500

    Creo que funciona, pero ajá

--- src/components/GradeCalcForm.jsx
+++ src/components/GradeCalcForm.jsx
@@ -1,12 +1,14 @@
 import { Plus, Calculator } from 'lucide-react'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useState } from 'react'
 import GradeInput from './GradeInput'
 
 
 const GradeCalcForm = () => {
     const [idCounter, setIdCounter] = useState(1);
-
+    const [whishedGrade, setWishedGrade] = useState(3);
+    const [neededGrade, setNeededGrade] = useState(3);
+    const [missingPercent, setMissingPercent] = useState(100);
     const [grades, setGrades] = useState([{
         id: 0,
         value: 0,
@@ -17,6 +19,19 @@ const GradeCalcForm = () => {
         setGrades([...grades, { id: idCounter, value: 0, percent: 0 }])
         setIdCounter(idCounter + 1)
         console.log(grades);
+    }
+
+    const calcNeededGrade = () => {
+        let actualGrade = 0;
+        let actualPercent = 0
+
+        grades.forEach(grade => {
+            actualGrade += grade.value * (grade.percent / 100);
+            actualPercent += (grade.percent / 100);
+        });
+        setNeededGrade((whishedGrade - actualGrade) / (1 - actualPercent))
+        setMissingPercent((1 - actualPercent) * 100)
+        actualPercent < 1 ? console.log(`Necesitas ${neededGrade} en el ${missingPercent}% restante`) : null;
     }
 
     return (
@@ -35,8 +50,7 @@ const GradeCalcForm = () => {
                     Añadir nota
                 </button>
                 <button className='flex items-center justify-center gap-2 p-2 md:w-1/4 text-white bg-sky-500 hover:bg-sky-400 transition rounded-md'
-                    onClick={() => {
-                    }}
+                    onClick={calcNeededGrade}
                 >
                     <Calculator />
                     Calcular restante
