818f8305662f2d9ecf85fd24579b0eb016de1ec4
juanmayorquin <juanjmayor@outlook.es>
2024-11-22 23:28:53 -0500

    Corregido el cálculo

--- src/components/GradeInput.jsx
+++ src/components/GradeInput.jsx
@@ -1,74 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import { X } from 'lucide-react'
+import React from "react";
+import { X } from "lucide-react";
 
 const GradeInput = ({ index, grades, setGrades }) => {
-    const id = grades[index].id;
-    const [localGradeValue, setLocalGradeValue] = useState(0);
-    const [localGradePercent, setLocalGradePercent] = useState(0);
+  const id = grades[index].id;
 
-    const writeFromGradesList = () => {
-        const gradeTextInput = document.getElementById(`nota-${id}`);
-        const percentTextInput = document.getElementById(`porcentaje-${id}`);
+  const handleGradeChange = (e) => {
+    let newGradesList = [...grades];
+    newGradesList[index].value = e.target.value;
+    setGrades(newGradesList);
+  };
 
-        gradeTextInput.value = grades[index].value != 0 ? grades[index].value : "";
-        percentTextInput.value = grades[index].percent != 0 ? grades[index].percent : "";
-    }
+  const handlePercentChange = (e) => {
+    let newGradesList = [...grades];
+    newGradesList[index].percent = e.target.value;
+    setGrades(newGradesList);
+  };
 
-    const writeToGradesList = () => {
-        const newGradesList = [...grades];
-        newGradesList[index].value = localGradeValue;
-        newGradesList[index].percent = localGradePercent;
-        setGrades(newGradesList);
-    }
+  const handleDelete = () => {
+    grades.length > 1
+      ? setGrades(grades.filter((grade) => grade.id !== id))
+      : setGrades([{ id: 0, value: 0, percent: 0 }]);
+  };
 
-    useEffect(() => {
-        writeFromGradesList();
-    }, [grades])
+  return (
+    <div className="flex gap-[10%] hover:bg-white/5 py-1 px-5 rounded-md transition-all">
+      <div className="flex flex-col py-2 w-1/2 group/grade">
+        <label
+          className="text-sm text-slate-100 group-focus-within/grade:text-sky-400 transition-all ease-in-out"
+          htmlFor={`nota-${id}`}
+        >
+          Nota {index + 1}{" "}
+        </label>
+        <input
+          id={`nota-${id}`}
+          placeholder="Ingresa la nota"
+          className="bg-transparent outline-none border-b py-0.5 focus:border-b-2 focus:p-1 focus:border-sky-500 focus:outline-none transition-all ease-in-out"
+          type="number"
+          onChange={handleGradeChange}
+        />
+      </div>
+      <div className="flex flex-col py-2 w-1/2 group/percent">
+        <label
+          className="text-sm text-slate-100 group-focus-within/percent:text-sky-400 transition-all ease-in-out"
+          htmlFor={`porcentaje-${id}`}
+        >
+          Porcentaje {index + 1} (%)
+        </label>
+        <input
+          id={`porcentaje-${id}`}
+          placeholder="Ingresa el porcentaje"
+          className="bg-transparent outline-none border-b py-0.5 focus:border-b-2 focus:p-1 focus:border-sky-500 focus:outline-none transition-all ease-in-out"
+          type="number"
+          onChange={handlePercentChange}
+        />
+      </div>
+      <button
+        className="text-white hover:text-red-500 transition-all ease-in-out"
+        type="button"
+        onClick={handleDelete}
+      >
+        <X />
+      </button>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        writeToGradesList();
-    }, [localGradeValue, localGradePercent])
-
-
-    const handleGradeChange = (e) => {
-        setLocalGradeValue(e.target.value);
-    };
-    const handlePercentChange = (e) => {
-        setLocalGradePercent(e.target.value)
-    };
-
-    const handleDelete = () => {
-        grades.length > 1 ? setGrades(grades.filter(grade => grade.id !== id)) : 0;
-        console.log(grades);
-    };
-
-    return (
-        <div className='flex gap-[10%] hover:bg-slate-700 py-1 px-5 rounded-md transition-all'>
-            <div className='flex flex-col py-2 w-1/2 group/grade'>
-                <label className='text-sm text-slate-100 group-focus-within/grade:text-sky-400 transition-all ease-in-out' htmlFor={`nota-${id}`}>Nota {index + 1} </label>
-                <input id={`nota-${id}`}
-                    placeholder='Ingresa la nota'
-                    className='bg-transparent outline-none border-b py-0.5 focus:border-b-2 focus:p-1 focus:border-sky-500 focus:outline-none transition-all ease-in-out'
-                    type="number"
-                    onChange={handleGradeChange}
-                />
-
-            </div>
-            <div className='flex flex-col py-2 w-1/2 group/percent'>
-                <label className='text-sm text-slate-100 group-focus-within/percent:text-sky-400 transition-all ease-in-out' htmlFor={`porcentaje-${id}`}>Porcentaje {index + 1} (%)</label>
-                <input id={`porcentaje-${id}`}
-                    placeholder='Ingresa el porcentaje'
-                    className='bg-transparent outline-none border-b py-0.5 focus:border-b-2 focus:p-1 focus:border-sky-500 focus:outline-none transition-all ease-in-out'
-                    type="number"
-                    onChange={handlePercentChange}
-                />
-            </div>
-            <button className='text-white hover:text-red-500 transition-all ease-in-out' type="button"
-                onClick={handleDelete}>
-                <X />
-            </button>
-        </div>
-    )
-}
-
-export default GradeInput
+export default GradeInput;
